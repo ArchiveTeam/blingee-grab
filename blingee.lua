07a6385e0ead0367c73e2da04b75a6209d9ed9ea
Gary Herreman <gary@garyrh.com>
2015-08-15 23:16:57 -0700

    Oops.

--- blingee.lua
+++ blingee.lua
@@ -153,6 +153,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     -- but just in case
     elseif string.match(newurl, "%.swf") then
       return newurl
+    else
       return nil
     end
   end
