95396b2a8a56cae716c7e57011b49eb940d5e604
Gary Herreman <gary@garyrh.com>
2015-08-23 23:54:54 -0700

    Add code for scraping stamp swfs.
    Adapted from julescarbon/blingee-stamp
    
    TODO: Make the page_loop try exactly how many pages are listed.
          Currently, it only tries 50 pages (existent or not) and stops.
          Unfortunately, apparently not all the stamps are listed in the
          embedded_list page(s), meaning we can't get some stamp swfs.

--- pipeline.py
+++ pipeline.py
@@ -16,6 +16,9 @@ import sys
 import time
 import string
 import requests
+import re
+from base64 import b64decode
+from Crypto.Cipher import AES
 from lxml import etree
 
 import seesaw
@@ -53,6 +56,42 @@ WGET_LUA = find_executable(
 if not WGET_LUA:
     raise Exception("No usable Wget+Lua found.")
 
+###########################################################################
+# The version number of this pipeline definition.
+#
+# Update this each time you make a non-cosmetic change.
+# It will be added to the WARC files and reported to the tracker.
+VERSION = "20150821.02"
+TRACKER_ID = 'blingee'
+TRACKER_HOST = 'tracker.archiveteam.org'
+# Number of blingees per item
+NUM_BLINGEES = 100
+# Number of profiles per item
+NUM_PROFILES = 10
+# Number of stamps per item
+NUM_STAMPS = 20
+
+USER_AGENTS = ['Mozilla/5.0 (Windows NT 6.3; rv:24.0) Gecko/20100101 Firefox/39.0',
+               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/39.0',
+               'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+               'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+               'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+               'Opera/9.80 (Windows NT 6.0; rv:2.0) Presto/2.12.388 Version/12.16',
+               'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36']
+USER_AGENT = random.choice(USER_AGENTS)
+REQUESTS_HEADERS = {"User-Agent": USER_AGENT, "Accept-Encoding": "gzip"}
+myparser = etree.HTMLParser(encoding="utf-8")
+
+###########################################################################
+# Stamp grabbing variables
+# Apparently Blingee has a limit of 9 tags on their tag search(?)
+LIMIT = 9
+# The ciphertext needs to be base64-decoded but *not* the key. wat.
+# Also, only the first 16 bytes of the key are needed.
+key = 'rAI1P8bpXoReutED8XOTT0lh26MWhWz87IH4t39LjJp3wxLkEHDKE2Er'[:16]
+cipher = AES.new(key, AES.MODE_ECB)
+BLOCK_SIZE = 16
+
 def base36_encode(n):
     """
     Encode integer value `n` using `alphabet`. The resulting string will be a
@@ -72,28 +111,124 @@ def base36_encode(n):
     r.reverse()
     return ''.join(r)
 
-###########################################################################
-# The version number of this pipeline definition.
-#
-# Update this each time you make a non-cosmetic change.
-# It will be added to the WARC files and reported to the tracker.
-VERSION = "20150821.02"
-TRACKER_ID = 'blingee'
-TRACKER_HOST = 'tracker.archiveteam.org'
-# Number of blingees per item
-NUM_BLINGEES = 100
-# Number of profiles per item
-NUM_PROFILES = 10
 
-USER_AGENTS = ['Mozilla/5.0 (Windows NT 6.3; rv:24.0) Gecko/20100101 Firefox/39.0',
-               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/39.0',
-               'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
-               'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
-               'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
-               'Opera/9.80 (Windows NT 6.0; rv:2.0) Presto/2.12.388 Version/12.16',
-               'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36']
-USER_AGENT = random.choice(USER_AGENTS)
-REQUESTS_HEADERS = {"User-Agent": USER_AGENT, "Accept-Encoding": "gzip"}
+def get_url(url):
+    tries = 0
+    while tries < 50:
+        try:
+            html = requests.get(url, headers=REQUESTS_HEADERS)
+        except requests.ConnectionError:
+            print("Got a connection error getting url, sleeping...")
+            sys.stdout.flush()
+            time.sleep(5)
+            tries += 1
+            continue
+        if html.status_code == 200 and html.text:
+            return html.text
+        else:
+            print("Got status code {0}, sleeping...".format(html.status_code))
+            sys.stdout.flush()
+            time.sleep(5)
+            tries += 1
+    raise Exception("Failed downloading {0}".format(url))
+
+
+def get_list(query, stamp_id, args=""):
+    url = "http://blingee.com/stamp/embedded_list?query={0}{1}".format(query, args)
+    search = get_url(url)
+    tree = etree.HTML(search, parser=myparser)
+    if 'galleryItem_{0}"'.format(stamp_id) in search:
+        ciphers = tree.xpath("//div[@class='list-action_link']/a/@onclick")
+        for cipher in ciphers:
+            if 'galleryItem_{0}"'.format(stamp_id) in cipher:
+                cipher = re.findall("addStampToBlingeeMaker\('([^']+)'\)", cipher)
+                if cipher:
+                    return cipher[0]
+    return ""
+
+
+def page_loop(query, stamp_id):
+    #print "Trying at most 10 pages..."
+    for page in xrange(1, 50):
+        cipher = get_list(query, stamp_id, "&page={0}".format(page))
+        if cipher:
+            return cipher
+    return ""
+
+
+def get_ciphertext(stamp_id, html):
+    if html:
+        if "Oops, Error" in html or "Account Login" in html:
+            return "Denied"
+        else:
+            tree = etree.HTML(html, parser=myparser)
+
+    title = tree.xpath("//div[@id='picdisplay_wrapper']/h1/text()")
+    title = title[0].encode('utf-8') or ""
+    title = title.replace(" ", "+")
+    tags = tree.xpath("//*[@id='picdetails']/dl/dd[3]/div/a/text()")
+
+    # Make sure space become + in title/tags
+    tags = " ".join(tags).encode('utf-8').split(" ")
+    tags_str = "+".join(" ".join(tags).split(" ")[:LIMIT]) or ""
+    title = title.split("+")
+    title_str = "+".join(title)
+    if tags_str:
+        title_tags = "{0}+{1}".format(title_str, "+".join(tags[:LIMIT-len(title)]))
+    else:
+        title_tags = ""
+
+    #print tags_str or title_str or stamp_id
+    for query in [tags_str, title_str, title_tags]:
+	if query:
+            #print query
+            cipher = get_list(query, stamp_id)
+            if cipher:
+                return cipher
+
+    # One last try (with quotes around title now.)
+    tags_str = "+".join(tags[:8])
+    if tags_str:
+        tags_str = "+{0}".format(tags_str)
+    else:
+        tags_str = ""
+    cipher = page_loop('"{0}"{1}'.format(title_str, tags_str), stamp_id)
+    if cipher:
+        return cipher
+    else: 
+        return "Unknown"
+
+
+def decrypt(ciphertext):
+    ciphertext = ciphertext.decode('base64')
+    decrypted = cipher.decrypt(ciphertext)
+
+    # Ciphertext padding is simply the byte
+    # chr(BLOCK_SIZE-(len(plaintext)%BLOCK_SIZE)) repeated 
+    # that many times.
+    end = len(decrypted)-ord(decrypted[-1])
+    plaintext = decrypted[0:end]
+    return plaintext
+
+def stamp_scraper(min_stamp_id, max_stamp_id):
+    min_stamp_id = min_stamp_id
+    max_stamp_id = max_stamp_id
+    urls = []
+    for stamp_id in xrange(min_stamp_id, max_stamp_id):
+        print("Finding stamp swf with ID {0}".format(str(stamp_id)))
+        stamp_page = get_url("http://blingee.com/stamp/view/{0}".format(str(stamp_id)))
+        ciphertext = get_ciphertext(stamp_id, stamp_page)
+
+        # Found the ciphertext!
+        if ciphertext != "Denied" and ciphertext != "Unknown":
+            plaintext = decrypt(ciphertext)
+            # Get the original swf only (skip the thumbnail.)
+            swf_urls = re.findall('swfHref="(http[^"]+)', plaintext)
+            urls.extend(swf_urls)
+        # Couldn't find the ciphertext. :(
+        else:
+            print("Skipping {0}, error is: {1}".format(str(stamp_id), ciphertext))
+    return urls
 
 ###########################################################################
 # This section defines project-specific tasks.
@@ -239,7 +374,10 @@ class WgetArgs(object):
                 wget_args.append("http://blingee.com/blingee/{0}/comments".format(val))
                 wget_args.append("http://bln.gs/b/{0}".format(base36_encode(val)))
         elif item_type == 'stamp':
-            wget_args.append("http://blingee.com/stamp/view/{0}".format(item_value))
+            for val in xrange(int(item_value), int(item_value)+NUM_STAMPS):
+                wget_args.append("http://blingee.com/stamp/view/{0}".format(val))
+            urls = stamp_scraper(int(item_value), int(item_value)+NUM_STAMPS)
+            wget_args.extend(urls)
         elif item_type == 'group':
             wget_args.extend(["--recursive", "--level=inf"])
             wget_args.append("http://blingee.com/group/{0}".format(item_value))
@@ -257,33 +395,22 @@ class WgetArgs(object):
             for val in xrange(int(item_value), int(item_value)+NUM_PROFILES):
                 print("Getting username for ID {0}...".format(val))
                 sys.stdout.flush()
-                tries = 0
-                while tries < 50:
-                    url = "http://blingee.com/badge/view/42/user/{0}".format(val)
-                    html = requests.get(url, headers=REQUESTS_HEADERS)
-                    if html.status_code == 200 and html.text:
-                        myparser = etree.HTMLParser(encoding="utf-8")
-                        tree = etree.HTML(html.text, parser=myparser)
-                        links = tree.xpath('//div[@id="badgeinfo"]//a/@href')
-                        username = [link for link in links if "/profile/" in link]
-                        if not username:
-                            print("Skipping deleted/private profile.")
-                            break
-                        else:
-                            username = username[0]
-                            wget_args.append("http://blingee.com{0}".format(username))
-                            wget_args.append("http://blingee.com{0}/statistics".format(username))
-                            wget_args.append("http://blingee.com{0}/circle".format(username))
-                            wget_args.append("http://blingee.com{0}/badges".format(username))
-                            wget_args.append("http://blingee.com{0}/comments".format(username))
-                            print("Username is {0}".format(username.replace("/profile/", "")))
-                            sys.stdout.flush()
-                            break
-                    else:
-                        print("Got status code {0}, sleeping...".format(html.status_code))
-                        sys.stdout.flush()
-                        time.sleep(5)
-                        tries += 1
+                url = "http://blingee.com/badge/view/42/user/{0}".format(val)
+                html = get_url(url)
+                tree = etree.HTML(html, parser=myparser)
+                links = tree.xpath('//div[@id="badgeinfo"]//a/@href')
+                username = [link for link in links if "/profile/" in link]
+                if not username:
+                    print("Skipping deleted/private profile.")
+                else:
+                    username = username[0]
+                    wget_args.append("http://blingee.com{0}".format(username))
+                    wget_args.append("http://blingee.com{0}/statistics".format(username))
+                    wget_args.append("http://blingee.com{0}/circle".format(username))
+                    wget_args.append("http://blingee.com{0}/badges".format(username))
+                    wget_args.append("http://blingee.com{0}/comments".format(username))
+                    print("Username is {0}".format(username.replace("/profile/", "")))
+                    sys.stdout.flush()
 
         else:
             raise Exception('Unknown item')
--- warrior-install.sh
+++ warrior-install.sh
@@ -10,6 +10,11 @@ then
   echo "Installing python-lxml"
   sudo apt-get -y install python-lxml
 fi
+if ! dpkg-query -Wf'${Status}' python-crypto 2>/dev/null | grep -q '^i'
+then
+  echo "Installing python-crypto"
+  sudo apt-get -y install python-crypto
+fi
 
 exit 0
 
